fa95304496406402efdaa0f05ece55aa0e5a8adc
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-21 07:44:19 +0530

    variable

---
+++ variable_declration.js
@@ -0,0 +1,12 @@
+name = "dantani prince pintubhai";
+age = "18";
+salary = "100000.00000"
+x=null;
+y=undefined;
+isfollow=true;
+console.log(isfollow)
+let a=10;
+console.log(a)
+let b;
+b=990;
+console.log(b)
