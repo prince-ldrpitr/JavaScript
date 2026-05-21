0c1b88cf26e52036671a48b6b5040f6819ae092c
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-21 07:41:20 +0530

    datatypes_js

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+name = "dantani prince pintubhai";
+age = "18";
+console.log(typeof name)
+
+
+
+const student = {
+    name : "dantani prince pintubhai",
+    age : 18,
+    branch : "BE_IT",
+};
+student["age"] = student["age"] + 1;
+
+console.log(student);
+console.log(student.name);
+console.log(student["age"]);
+console.log(typeof student["name"]);
