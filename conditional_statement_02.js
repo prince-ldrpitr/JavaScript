2a5747480ee629ccc68e1119604dae70e83d10d2
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-28 07:50:57 +0530

    if else

---
+++ conditional_statement_02.js
@@ -0,0 +1,14 @@
+// odd and even number 
+
+let num = 10;
+if(num%2==0){
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
+
+//ternary operator
+num = 13
+let result = num%2==0 ? "even number" : "odd number";
+console.log(result)
