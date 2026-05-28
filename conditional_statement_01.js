4f109fd697bdedf46c4e97646822bd6dc61a7988
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-28 07:50:17 +0530

    if else

---
+++ conditional_statement_01.js
@@ -0,0 +1,24 @@
+/*
+let mode = "dark";
+let color;
+
+if(mode == "dark"){
+    color = "black";
+}
+
+if(mode == "light"){
+    color = "white";
+}
+
+console.log(color);
+*/
+
+let mode = "dark";
+let color;
+if(mode == "dark"){
+    color = "black";
+}
+else{
+    color = "white"
+}
+console.log(color)
