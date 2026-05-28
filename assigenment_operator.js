5c32519b0cea5ec2a3dd2fab1b8cf8b2e423c15e
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-28 07:48:59 +0530

    assigenment operator

---
+++ assigenment_operator.js
@@ -0,0 +1,18 @@
+/* 
+let a=33;
+a += 4;
+console.log(a)
+a-=4;
+console.log(a)*/
+
+// comparison operator
+
+let a = 33;
+let b = 32;
+
+console.log("a==b",a==b);
+console.log("a!=b",a!=b);
+
+let con1 = a>b;
+let con2 = a==33;
+console.log("con1 && con2",con1 && con2); 
