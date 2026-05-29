1e9128fba498078c5f26ec487742c34fdab48e5a
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-29 08:32:05 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,13 @@
+// arithmetic operator
+let a=7;
+let b=2;
+console.log("a = ",a,"b = ",b);
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);
+
+
+// incerment and decrement operator is also called unary operator
