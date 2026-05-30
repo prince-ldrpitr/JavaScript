19c2373ceeaa8a81eeb57fa2898a46e49497e590
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-30 12:28:22 +0530

    array

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let marks = [96,36,59,88,56]
+console.log(marks.length)
+console.log(marks[0])
+
+
+let heros = ["hritik","akshay","salman","sarukh","sunny"]
+for(i=0;i<heros.length;i++){
+    console.log(heros[i]);
+}
