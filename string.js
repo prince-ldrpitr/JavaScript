b79f0bd21b3c1e2367329c798b2bfb536e0b905c
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-30 12:29:22 +0530

    strinmg

---
+++ string.js
@@ -0,0 +1,13 @@
+// javascript ma string immutable hotui hain or user function use karke vo hame ek new string return hoti hain original string main kabi change nahi hoti
+
+let str = "prince";
+console.log(str.length);
+console.log(str[0]);
+console.log(str.toUpperCase());
+console.log(str.trim());
+
+let str1 =  "krishna";
+let str2 = "dantani";
+console.log(str.slice(0,3));
+console.log(str1.concat(str2));
+console.log(str1.replace("krishna","prince"))
