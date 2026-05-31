e60cc5e26f33b7cd10d4bfdaeef9d96496d44725
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-05-31 08:56:44 +0530

    taking input

---
+++ taking_input.js
@@ -0,0 +1,12 @@
+//alert("hello!");
+/*
+let name = prompt("hello!");
+console.log(name);*/
+
+let num = prompt("enter a number");
+if(num%5==0){
+    console.log("the number is multiple by 5");
+}
+else{
+    console.log("the number is not multiple by 5");
+}
