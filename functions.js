c639f74a5239ffc4647ae6b9283529b632836644
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-06-03 07:48:02 +0530

    function

---
+++ functions.js
@@ -0,0 +1,15 @@
+// without parameters
+function myfunction(){
+    console.log("hello1,my name is prince");
+    console.log("i am study in 3rd year");
+}
+
+myfunction();
+
+// with parameters
+
+function introduction(msg){
+    console.log(msg);
+}
+
+introduction("i love js");
