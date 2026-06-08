7ec564310b17e52144ce610ea031b5bbf9c05637
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-06-08 17:22:32 +0530

    if else statement

--- conditional_statement_01.js
+++ conditional_statement_01.js
@@ -22,3 +22,12 @@ else{
     color = "white"
 }
 console.log(color)
+
+
+let num = prompt("enter the number : ");
+if(num%5==0){
+    console.log("the number divisible by 5");
+}
+else{
+    console.log("the number is not divisible by 5");
+}
