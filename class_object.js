9bbf02b96e314aa066311d3d441d0c29502c8849
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-06-12 10:11:28 +0530

    class

---
+++ class_object.js
@@ -0,0 +1,14 @@
+class car{
+    start(){
+        console.log("car started");
+    }
+
+    stop(){
+        console.log("car stoped");
+    }
+}
+
+
+let thar = new car();
+thar.start();
+thar.stop();
